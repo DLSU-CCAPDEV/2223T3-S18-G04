e8eaf03c0eab4b6589e956512bca53af715ce53c
Sandeon Gavan <sandeon_gavan@dlsu.edu.ph>
2023-07-22 16:48:27 +0800

    added reservationController.js not yet implemented

---
+++ mco-phase2-ver2/controllers/reservationController.js
@@ -0,0 +1,21 @@
+// import module `database` from `../models/db.js`
+const db = require('../models/db.js');
+
+// import module `User` from `../models/UserModel.js`
+const User = require('../models/user.js');
+
+if (typeof localStorage === "undefined" || localStorage === null) {
+    var LocalStorage = require('node-localstorage').LocalStorage;
+    localStorage = new LocalStorage('./scratch');
+  }
+
+const reservationController = {
+
+        getReservation: function(req, res) {
+            localStorage.clear();
+            res.render('')
+        }
+
+}
+
+module.exports = reservationController;
