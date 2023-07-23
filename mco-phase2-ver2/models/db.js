dbb326e26c1b4a82181c72be76a3bc5790c021a6
BienMags <134592365+BeinnerDaps@users.noreply.github.com>
2023-07-23 22:12:47 +0800

    Update db.js
    added if else to not include projection if it is null

--- mco-phase2-ver2/models/db.js
+++ mco-phase2-ver2/models/db.js
@@ -18,7 +18,7 @@ const url = 'mongodb://127.0.0.1:27017/mco';
 const dbName = 'database';
 
 // additional connection options
-const options = { 
+const options = {
     useUnifiedTopology: true,
     useNewUrlParser: true
  };
@@ -36,14 +36,14 @@ const database = {
     insertOne: async function(model, doc) {
         return await model.create(doc);
     },
-    
+
         /*
             inserts multiple `docs` to the database based on the model `model`
         */
     insertMany: async function(model, docs) {
         return await model.insertMany(docs);
     },
-    
+
     /*
             searches for a single document based on the model `model`
             filtered through the object `query`
@@ -51,9 +51,13 @@ const database = {
             callback function is called after the execution of findOne() function
     */
     findOne: async function(model, query, projection) {
-        return await model.findOne(query, projection);
+        if (projection === null) {
+            return await model.find(query);
+        } else {
+            return await model.find(query, projection);
+        }
     },
-    
+
         /*
             searches for multiple documents based on the model `model`
             filtered through the object `query`
@@ -61,9 +65,13 @@ const database = {
             callback function is called after the execution of findMany() function
         */
     findMany: async function(model, query, projection) {
-        return await model.find(query, projection);
+        if (projection === null) {
+            return await model.find(query);
+        } else {
+            return await model.find(query, projection);
+        }
     },
-    
+
         /*
             updates the value defined in the object `update`
             on a single document based on the model `model`
@@ -72,7 +80,7 @@ const database = {
     updateOne: async function(model, filter, update) {
         return await model.updateOne(filter, update);
     },
-    
+
         /*
             updates the value defined in the object `update`
             on multiple documents based on the model `model`
@@ -81,7 +89,7 @@ const database = {
     updateMany: async function(model, filter, update) {
         return await model.updateMany(filter, update);
     },
-    
+
         /*
             deletes a single document based on the model `model`
             filtered using the object `conditions`
@@ -89,7 +97,7 @@ const database = {
     deleteOne: async function(model, conditions) {
         return await model.deleteOne(conditions);
     },
-    
+
         /*
             deletes multiple documents based on the model `model`
             filtered using the object `conditions`
@@ -97,7 +105,7 @@ const database = {
     deleteMany: async function(model, conditions) {
         return await model.deleteMany(conditions);
     }
-    
+
     }
 /*
     exports the object `database` (defined above)
