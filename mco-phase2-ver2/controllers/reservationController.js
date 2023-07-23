01341408a07f5d15362e8c6a390d9f2c59448070
BienMags <134592365+BeinnerDaps@users.noreply.github.com>
2023-07-24 03:27:06 +0800

    Update reservationController.js
    added addreserve, getallreserves, updatereserve, deletereserve

--- mco-phase2-ver2/controllers/reservationController.js
+++ mco-phase2-ver2/controllers/reservationController.js
@@ -7,14 +7,74 @@ const User = require('../models/user.js');
 // import module `Reservation` from `../models/reservation`
 const Reservation = require('../models/reservation.js');
 
+// import module `ObjectId`
+const ObjectId = require('mongodb').ObjectId;
+
 if (typeof localStorage === "undefined" || localStorage === null) {
     var LocalStorage = require('node-localstorage').LocalStorage;
     localStorage = new LocalStorage('./scratch');
   }
 
 const reservationController = {
-        getReservation: function(req, res) {
-            res.render('')
+       postNewreserve: async function(req, res) {
+            var email = localStorage.getItem('email');
+            var username = req.body.username;
+            var lab = req.body.lab;
+            var seatnum = req.body.seatnum;
+            var requestDateTime = req.body.requestDateTime;
+            var reserveDateTime = req.body.reserveDateTime;
+            var isAnonymous = req.body.isAnonymous;
+
+            var newreserve = {
+                email: email,
+                username: username,
+                labnum: lab,
+                seatnum: seatnum,
+                requestDateTime: requestDateTime,
+                reserveDateTime: reserveDateTime,
+                isAnonymous: isAnonymous,
+            }
+            console.log(newreserve);
+            var result = await db.insertOne(Reservation, newreserve);
+            console.log(result);
+            res.send(result);
+        },
+
+        getExistingreserve: async function (req, res) {
+            var query = {email: localStorage.getItem('email')};
+            var projection = ' _id username labnum seatnum requestDateTime reserveDateTime isAnonymous'
+            var result = await db.findMany(Reservation, query, projection);
+            console.log(result);
+            res.send(result);
+        },
+
+        postEditreserve: async function (req, res) {
+            var username = req.body.username;
+            var seatnum = req.body.seatnum;
+            var requestDateTime = req.body.requestDateTime;
+            var reserveDateTime = req.body.reserveDateTime;
+            var isAnonymous = req.body.isAnonymous;
+            var filter = req.body._id;
+
+            var update = {
+                username: username,
+                seatnum: seatnum,
+                requestDateTime: requestDateTime,
+                reserveDateTime: reserveDateTime,
+                isAnonymous: isAnonymous
+            }
+            var result = await db.updateOne(Reservation, filter, update)
+            console.log(result);
+            res.send(result);
+        },
+
+        deleteReserve: async function (req, res) {
+            const { id } = req.params;
+            const _id = ObjectId(id);
+
+            var result = await db.deleteOne(Reservation, _id);
+            console.log(result);
+            res.send(result);
         },
 
         seeReservations: async function (req, res) {
