562797fd8c5c47ce41f02aeff15ea8eca8fe9ee8
BienMags <134592365+BeinnerDaps@users.noreply.github.com>
2023-07-23 23:46:59 +0800

    Update routes.js

--- mco-phase2-ver2/routes/routes.js
+++ mco-phase2-ver2/routes/routes.js
@@ -54,11 +54,11 @@ app.get('/seeReservations', reservationController.seeReservations);
 
 app.get('/searchSlots', reservationController.searchSlots);
 
-app.use(express.json());
-app.post('/slot_availability/add_reserve', reservationController.postNewreserve);
-app.post('/slot_availability/edit_reserve', reservationController.postEditreserve);
-app.post('/slot_availability/delete_reservation', reservationController.deleteReserve);
-app.get('/slot_availability/get_reservations', reservationController.getExistingreserve);
+// app.use(express.json());
+// app.post('/slot_availability/add_reserve', reservationController.postNewreserve);
+// app.post('/slot_availability/edit_reserve', reservationController.postEditreserve);
+// app.post('/slot_availability/delete_reservation', reservationController.deleteReserve);
+// app.get('/slot_availability/get_reservations', reservationController.getExistingreserve);
 //app.get('/slot_availability/get_size', reservationController.getGetSize);
 
 module.exports = app;
