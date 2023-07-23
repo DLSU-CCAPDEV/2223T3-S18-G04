2791f047317b6a3c91b4e1f23723ed07382fe776
AntonioLuizVeloso <92433849+AntonioLuizVeloso@users.noreply.github.com>
2023-07-24 00:07:34 +0800

    Revert "Update reservation.js"
    This reverts commit abbdd2ac27dabf852121e23b6fab03d1c2de5fa8.

--- mco-phase2-ver2/models/reservation.js
+++ mco-phase2-ver2/models/reservation.js
@@ -24,7 +24,7 @@ const registerSchema = new Schema({
         required: [true, "lab number is required"]
     },
 
-    seat: {
+    seatnum: {
         type: Array,
         required: [true, "seat number is required"]
     },
