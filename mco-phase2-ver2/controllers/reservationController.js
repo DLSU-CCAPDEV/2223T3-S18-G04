fb15744cc9b482d399b770efd2a32be520c3a332
AntonioLuizVeloso <92433849+AntonioLuizVeloso@users.noreply.github.com>
2023-07-24 00:07:19 +0800

    Revert "Update reservationController.js"
    This reverts commit 8f36f76ca7022a2b0b5eaa09c086ac6fd0f739e8.

--- mco-phase2-ver2/controllers/reservationController.js
+++ mco-phase2-ver2/controllers/reservationController.js
@@ -13,66 +13,66 @@ if (typeof localStorage === "undefined" || localStorage === null) {
   }
 
 const reservationController = {
-        // postNewreserve: async function(req, res) {
-        //     try {
-        //         var email = localStorage.getItem('email');
-        //         var {reservenumber, username, lab, seat, requestdatetime, reservedate, time, isAnonymous} = req.body
-        //         if (!seat || reservedate === '' || time === '') {
-        //             res.status(400).json({ message: 'Error: Incomplete entries'});
-        //         } else {
-        //             var reservedatetime = reservedate + "T" + time;
-        //             const newreserve = {email, reservenumber, username, lab, seat, requestdatetime, reservedatetime, isAnonymous};
-        //             console.log(newreserve);
-        //             var res = await db.insertOne(Reservation, newreserve)
-        //                 .then(result => res.json(result))
-        //                 .catch(error => res.json(error));
-        //         }
-        //     } catch (error) {
-        //         console.error('Error:', error);
-        //         res.status(500).json({ error: 'An error occurred while fetching data.' });
-        //     }
-        // },
+        postNewreserve: async function(req, res) {
+            try {
+                var email = localStorage.getItem('email');
+                var {reservenumber, username, lab, seat, requestdatetime, reservedate, time, isAnonymous} = req.body
+                if (!seat || reservedate === '' || time === '') {
+                    res.status(400).json({ message: 'Error: Incomplete entries'});
+                } else {
+                    var reservedatetime = reservedate + "T" + time;
+                    const newreserve = {email, reservenumber, username, lab, seat, requestdatetime, reservedatetime, isAnonymous};
+                    console.log(newreserve);
+                    var res = await db.insertOne(Reservation, newreserve)
+                        .then(result => res.json(result))
+                        .catch(error => res.json(error));
+                }
+            } catch (error) {
+                console.error('Error:', error);
+                res.status(500).json({ error: 'An error occurred while fetching data.' });
+            }
+        },
     
-        // postEditreserve: async function (req, res) {
-        //     try {
-        //         var email = localStorage.getItem('email');
-        //         var {reservenumber, username, lab, seat, requestdatetime, reservedate, time, isAnonymous} = req.body
-        //         var reservedatetime = reservedate + "T" + time;
-        //         var filter = { reservenumber: reservenumber };
-        //         var update = {$set: { email, reservenumber, username, lab, seat, requestdatetime, reservedatetime, isAnonymous},};
-        //         var res = await db.updateOne(Reservation, filter, update)
-        //             .then(result => res.json(result))
-        //             .catch(error => res.json(error));
-        //     } catch (error) {   
-        //         console.error('Error:', error);
-        //         res.status(500).json({ error: 'An error occurred while fetching data.' });
-        //     }
-        // },
+        postEditreserve: async function (req, res) {
+            try {
+                var email = localStorage.getItem('email');
+                var {reservenumber, username, lab, seat, requestdatetime, reservedate, time, isAnonymous} = req.body
+                var reservedatetime = reservedate + "T" + time;
+                var filter = { reservenumber: reservenumber };
+                var update = {$set: { email, reservenumber, username, lab, seat, requestdatetime, reservedatetime, isAnonymous},};
+                var res = await db.updateOne(Reservation, filter, update)
+                    .then(result => res.json(result))
+                    .catch(error => res.json(error));
+            } catch (error) {   
+                console.error('Error:', error);
+                res.status(500).json({ error: 'An error occurred while fetching data.' });
+            }
+        },
     
-        // getExistingreserve: async function (req, res) {
-        //     try {
-        //         var query = {email: localStorage.getItem('email')};
-        //         var projection = 'email reservenumber username lab seat requestdatetime reservedatetime isAnonymous'
-        //         const specificlabs = await db.findMany(Reservation, query, projection);
-        //         res.json(specificlabs);
-        //     } catch (error) {
-        //         console.error('Error:', error);
-        //         res.status(500).json({ error: 'An error occurred while fetching data.' });
-        //     }
-        // },
+        getExistingreserve: async function (req, res) {
+            try {
+                var query = {email: localStorage.getItem('email')};
+                var projection = 'email reservenumber username lab seat requestdatetime reservedatetime isAnonymous'
+                const specificlabs = await db.findMany(Reservation, query, projection);
+                res.json(specificlabs);
+            } catch (error) {
+                console.error('Error:', error);
+                res.status(500).json({ error: 'An error occurred while fetching data.' });
+            }
+        },
     
-        // deleteReserve: function(req, res) {
-        //     try {
-        //         const {reservenumber} = req.body;
-        //         var filter = { reservenumber: reservenumber };
-        //         db.deleteOne(User, filter)
-        //             .then(result => res.json(result))
-        //             .catch(error => res.send(error));
-        //     } catch (error) {
-        //         console.error('Error:', error);
-        //         res.status(500).json({ error: 'An error occurred while fetching data.' });
-        //     }
-        // },
+        deleteReserve: function(req, res) {
+            try {
+                const {reservenumber} = req.body;
+                var filter = { reservenumber: reservenumber };
+                db.deleteOne(User, filter)
+                    .then(result => res.json(result))
+                    .catch(error => res.send(error));
+            } catch (error) {
+                console.error('Error:', error);
+                res.status(500).json({ error: 'An error occurred while fetching data.' });
+            }
+        },
     
         // getGetSize: async function (req, res) {
         //     try {
