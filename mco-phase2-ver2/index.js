e4a6b0a0af20bb5a1b71fbb6755a06df9e97d978
AntonioLuizVeloso <92433849+AntonioLuizVeloso@users.noreply.github.com>
2023-08-01 11:15:58 +0800

    changed index for website deployment

--- mco-phase2-ver2/index.js
+++ mco-phase2-ver2/index.js
@@ -1,23 +1,13 @@
-const dotenv = require('dotenv');
 const express = require('express');
 const path = require('path');
-const hbs = require('hbs');
 const db = require('./models/db.js');
 const routes = require('./routes/routes.js');
 
-var bodyParser = require('body-parser');
-
 const app = express();
-
-var jsonParser = bodyParser.json();
-
-dotenv.config();
-port = process.env.PORT;
-hostName = process.env.HOSTNAME;
-
-var currentUser;
+const port = 9090;
 
 app.set('view engine', 'hbs');
+app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', routes);
@@ -25,6 +15,5 @@ app.use('/', routes);
 db.connect();
 
 app.listen(port, hostName, function() {
-    console.log('Server running at: ');
-    console.log('http://' + hostName + ':' + port);
+    console.log('App listening at port ' + port);
 });
