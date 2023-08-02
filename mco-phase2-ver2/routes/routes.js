28d03e9feb7c402a7b9dd7a4cd1266c438f5a9c9
BienMags <134592365+BeinnerDaps@users.noreply.github.com>
2023-08-02 14:20:00 +0800

    Added admin and sorted each controller in their own groups

--- mco-phase2-ver2/routes/routes.js
+++ mco-phase2-ver2/routes/routes.js
@@ -24,11 +24,15 @@ var urlencodedParser = bodyParser.urlencoded({extended: true});
 
 const upload = multer({storage: storage});
 
+// ------------------------------------- SECURITY CONTROLLERS -------------------------------------
+
 app.get(['/', '/login'], securityController.getLogin);
 app.post(['/', '/login'], urlencodedParser, securityController.postLogin);
 
 app.get('/register', securityController.getRegister);
 app.post('/register', urlencodedParser, securityController.postRegister);
+
+// ------------------------------------- PROFILE CONTROLLERS -------------------------------------
 
 app.get('/user_profile', profileController.getProfile);
 
@@ -41,9 +45,11 @@ app.post('/edit_pfp', upload.single("pfp"), profileController.postEditPfp);
 app.get('/delete_account', profileController.getDeleteAccount);
 app.post('/delete_account', urlencodedParser, profileController.postDeleteAccount);
 
-app.get('/slot_search', reservationController.getSlotSearch);
-
 app.get('/getSearchUsers', profileController.getSearchAccount);
+
+// ------------------------------------- RESERVATION CONTROLLERS -------------------------------------
+
+app.get('/slot_search', reservationController.getSlotSearch);
 
 app.get('/seeReservations', reservationController.seeReservations);
 
@@ -52,12 +58,24 @@ app.get('/searchSlots', reservationController.searchSlots);
 app.get('/getSAReservations', reservationController.getAllReserves);
 
 app.use(express.json());
+
 app.get('/slot_availability', urlencodedParser, reservationController.getSlotAvailability);
+
 app.post('/slot_availability/add_reserve', reservationController.postNewreserve);
+
 app.get('/slot_availability/get_reservations', reservationController.getExistingreserve);
+
 app.post('/slot_availability/edit_reserve', reservationController.postEditreserve);
+
 app.delete('/slot_availability/:id', reservationController.deleteReserve);
+
 app.post('/slot_availability/seats_postget', reservationController.postgetSeats);
+
+app.get('/slot_availability/get_emails', reservationController.getEmails);
+
+app.get('/slot_availability/checkadmin', reservationController.checkAdmin);
+
+// ------------------------------------- OTHER CONTROLLERS -------------------------------------
 
 app.get('/about', controller.getAbout);
 
