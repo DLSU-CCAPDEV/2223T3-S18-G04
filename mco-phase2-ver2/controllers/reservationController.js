658e2c3bc52a3a484c5d73206fabff14a06238dc
BienMags <134592365+BeinnerDaps@users.noreply.github.com>
2023-08-02 14:16:46 +0800

    Updated so that admin is considered

--- mco-phase2-ver2/controllers/reservationController.js
+++ mco-phase2-ver2/controllers/reservationController.js
@@ -24,112 +24,144 @@ const reservationController = {
         res.render('slot_availability');
     }, 
 
+    seeReservations: async function (req, res) {
+        if(req.query.email != '') {
+            var query = {email: req.query.email};
+        } else {var query = {email: localStorage.getItem('email')};}
+        console.log(query);
+        var projection = 'labnum seatnum requestDateTime reserveDateTime'
+
+        var result = await db.findMany(Reservation, query, projection);
+        console.log(result);
+        res.send(result);
+    },
+
+    searchSlots: async function (req, res) {
+        var labnum = req.query.labnum;
+        var reserveDateTime = req.query.reserveDateTime;
+        var projection = 'labnum seatnum reserveDateTime';
+
+        var result = await db.findMany(Reservation, {labnum: labnum, reserveDateTime: reserveDateTime}, projection);
+        res.send(result);
+    },
+
     postNewreserve: async function(req, res) {
-        var email = localStorage.getItem('email');
+        var email = req.body.email;
+        if (email == null) {
+            email = localStorage.getItem('email');
+        }
         var username = req.body.username;
         var lab = req.body.lab;
         var seatnum = req.body.seatnum;
         var requestDateTime = req.body.requestDateTime;
         var reserveDateTime = req.body.reserveDateTime;
         var isAnonymous = req.body.isAnonymous;
+        var markasDone = req.body.markasDone;
 
-            var newreserve = {
-                email: email,
-                username: username,
-                labnum: lab,
-                seatnum: seatnum,
-                requestDateTime: requestDateTime,
-                reserveDateTime: reserveDateTime,
-                isAnonymous: isAnonymous,
-            }
-            //console.log(newreserve);
-            var result = await db.insertOne(Reservation, newreserve);
-            //console.log(result);
-            res.send(result);
-        },
-
-        getExistingreserve: async function (req, res) {
-            var query = {email: localStorage.getItem('email')};
-            var projection = ' _id username labnum seatnum reserveDateTime'
-            var result = await db.findMany(Reservation, query, projection);
-            //console.log(result);
-            res.send(result);
-        },
-
-        getAllReserves: async function (req, res) {
-            var projection = ' _id email username labnum seatnum requestDateTime reserveDateTime isAnonymous'
-            var result = await db.findMany(Reservation, req.query, projection);
-            //console.log(result);
-            res.send(result);
-        },
-
-        postEditreserve: async function (req, res) {
-            var username = req.body.username;
-            var seatnum = req.body.seatnum;
-            var requestDateTime = req.body.requestDateTime;
-            var reserveDateTime = req.body.reserveDateTime;
-            var isAnonymous = req.body.isAnonymous;
-            var id = req.body._id;
-            var idtoupdate = new ObjectId(id);
-
-            var update = {
-                username: username,
-                seatnum: seatnum,
-                requestDateTime: requestDateTime,
-                reserveDateTime: reserveDateTime,
-                isAnonymous: isAnonymous
-            }
-            console.log(update);
-            var result = await db.updateOne(Reservation, { _id: idtoupdate }, update)
-            console.log(result);
-            res.send(result);
-        },
-
-        deleteReserve: async function (req, res) {
-            var id = req.params.id;
-            var idtodelete = new ObjectId(id);
-
-            var result = await db.deleteOne(Reservation, { _id: idtodelete });
-            //console.log(result);
-            res.send(result);
-        },
-
-        postgetSeats: async function (req, res) {
-            var seatlab = req.body.labseat;
-            var datetime = req.body.datetime;
-            var query = {
-                labnum: seatlab,
-                reserveDateTime: datetime
-            };
-            var projection = ' seatnum '
-
-            var result = await db.findMany(Reservation, query, projection);
-            console.log("postgetSeats");
-
-            console.log(result);
-            res.send(result);
-        },
-
-        seeReservations: async function (req, res) {
-            if(req.query.email != '') {
-                var query = {email: req.query.email};
-            } else {var query = {email: localStorage.getItem('email')};}
-            console.log(query);
-            var projection = 'labnum seatnum requestDateTime reserveDateTime'
-
-            var result = await db.findMany(Reservation, query, projection);
-            console.log(result);
-            res.send(result);
-        },
-
-        searchSlots: async function (req, res) {
-            var labnum = req.query.labnum;
-            var reserveDateTime = req.query.reserveDateTime;
-            var projection = 'labnum seatnum reserveDateTime';
-
-            var result = await db.findMany(Reservation, {labnum: labnum, reserveDateTime: reserveDateTime}, projection);
-            res.send(result);
+        var newreserve = {
+            email: email,
+            username: username,
+            labnum: lab,
+            seatnum: seatnum,
+            requestDateTime: requestDateTime,
+            reserveDateTime: reserveDateTime,
+            isAnonymous: isAnonymous,
+            markasDone: markasDone
         }
+        var result = await db.insertOne(Reservation, newreserve);
+        //console.log(result);
+        res.send(result);
+    },
+
+    getExistingreserve: async function (req, res) {
+        var currentemail = localStorage.getItem('email');
+        var adminemail = await db.findMany(User, {account_type: 'tech'}, ' email ');
+
+        if (adminemail.some(admin => admin.email === currentemail)) {
+            var query = {
+                //markasDone: false
+            };
+        } else {
+            var query = {
+                email: currentemail
+                //markasDone: false
+            };
+        }
+
+        var projection = ' _id email username labnum seatnum reserveDateTime '
+        var result = await db.findMany(Reservation, query, projection);
+        //console.log(result);
+        res.send(result);
+    },
+
+    getAllReserves: async function (req, res) {
+        var projection = ' _id email username labnum seatnum requestDateTime reserveDateTime isAnonymous'
+        var result = await db.findMany(Reservation, req.query, projection);
+        //console.log(result);
+        res.send(result);
+    },
+
+    postEditreserve: async function (req, res) {
+        var username = req.body.username;
+        var seatnum = req.body.seatnum;
+        var requestDateTime = req.body.requestDateTime;
+        var reserveDateTime = req.body.reserveDateTime;
+        var isAnonymous = req.body.isAnonymous;
+        var id = req.body._id;
+        var idtoupdate = new ObjectId(id);
+
+        var update = {
+            username: username,
+            seatnum: seatnum,
+            requestDateTime: requestDateTime,
+            reserveDateTime: reserveDateTime,
+            isAnonymous: isAnonymous
+        }
+        console.log(update);
+        var result = await db.updateOne(Reservation, { _id: idtoupdate }, update)
+        console.log(result);
+        res.send(result);
+    },
+
+    deleteReserve: async function (req, res) {
+        var id = req.params.id;
+        var idtodelete = new ObjectId(id);
+
+        var result = await db.deleteOne(Reservation, { _id: idtodelete });
+        res.send(result);
+    },
+
+    postgetSeats: async function (req, res) {
+        var seatlab = req.body.labseat;
+        var datetime = req.body.datetime;
+        var query = {
+            labnum: seatlab,
+            reserveDateTime: datetime
+        };
+        var projection = ' seatnum ';
+
+        var result = await db.findMany(Reservation, query, projection);
+        res.send(result);
+    },
+
+    getEmails: async function (req, res) {
+        var query = {account_type: 'student'};
+        var projection = ' email ';
+
+        var result = await db.findMany(User, query, projection);
+        console.log(result);
+        res.send(result);
+    },
+
+    checkAdmin: async function (req, res) {
+        var currentemail = localStorage.getItem('email');
+        var adminemail = await db.findMany(User, {account_type: 'tech'}, ' email ');
+        if (adminemail.some(admin => admin.email === currentemail)) {
+            res.send(true);
+        } else {
+            res.send(false);
+        }
+    }
 }
 
 module.exports = reservationController;
