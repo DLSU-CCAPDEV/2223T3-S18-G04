e310ec36eb8ca106e730e32b4cb2c67ed54736d2
BienMags <134592365+BeinnerDaps@users.noreply.github.com>
2023-08-03 17:41:52 +0800

    I forgot what I updated so I just reupdated the code

--- mco-phase2-ver2/public/scripts/reserve_admin.js
+++ mco-phase2-ver2/public/scripts/reserve_admin.js
@@ -1,6 +1,9 @@
 // ------------------------------------- CHECK IF CURRENT USER IS ADMIN -------------------------------------
 function AdminCheck() {
-    return fetch ('/slot_availability/checkadmin')
+    return fetch ('/slot_availability/checkadmin', {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+        })
         .then(response => response.json())
         .then(data => {return data;})
         .catch(error => {console.error('Error:', error); throw error;});
@@ -9,7 +12,10 @@ function AdminCheck() {
 // ------------------------------------- GET ALL STUDENT EMAILS -------------------------------------
 var studentemails = [];
 function getemails() {
-    fetch('/slot_availability/get_emails')
+    fetch('/slot_availability/get_emails', {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+    })
     .then(response => response.json())
     .then(dataArray => {
         studentemails = [].concat(...dataArray.map(item => item.email));;
@@ -24,14 +30,23 @@ function fetchstudentemails(studentemails) {
         emailcontent += `<option value="${email}"> ${email} </option>`;
     }
 
-    document.querySelectorAll("#labtechemailroot").forEach(element => {
-        element.innerHTML = `
-            <div class="textbox">
-                <select class="inputtext" name="labtechemail" id="labtechemail" size="1" placeholder="" required>
-                    ${emailcontent}
-                </select>
-                <i class="uil uil-envelope"></i>
-            </div>
-        `;
+    document.querySelectorAll("#labtechemailroot, #labtechemaileditroot").forEach(element => {
+        if (element.id === "labtechemailroot") {
+            element.innerHTML = `
+                <div class="textbox">
+                    <select class="inputtext" name="labtechemail" id="labtechemail" size="1" placeholder="" required>
+                        ${emailcontent}
+                    </select>
+                    <i class="uil uil-envelope"></i>
+                </div>`;
+        } else if (element.id === "labtechemaileditroot") {
+            element.innerHTML = `
+                <div class="textbox">
+                    <select class="inputtext" name="labtechemailedit" id="labtechemailedit" size="1" placeholder="" required disabled>
+                        ${emailcontent}
+                    </select>
+                    <i class="uil uil-envelope"></i>
+                </div>`;
+        }
     });
 }
