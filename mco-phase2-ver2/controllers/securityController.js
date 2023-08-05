0690f64e0585348f434297aaf21cb07c074b1f1f
Sandeon Gavan <sandeon_gavan@dlsu.edu.ph>
2023-08-05 16:38:14 +0800

    added pw hashing NOTE: plaintxt pw in db invalid

--- mco-phase2-ver2/controllers/securityController.js
+++ mco-phase2-ver2/controllers/securityController.js
@@ -4,6 +4,10 @@ const db = require('../models/db.js');
 // import module `User` from `../models/UserModel.js`
 const User = require('../models/user.js');
 
+//import module 'bcrypt'
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 if (typeof localStorage === "undefined" || localStorage === null) {
     var LocalStorage = require('node-localstorage').LocalStorage;
     localStorage = new LocalStorage('./scratch');
@@ -19,16 +23,25 @@ const securityController = {
         var email = req.body.email;
         var pw = req.body.password;
 
-        var query = {email: email, password: pw};
+        var query = {email: email}; //took off password: pw from query
 
         var projection = 'email password username description account_type';
 
         var response = await db.findOne(User, query, projection);
+        var dbPass = await db.findOne(User, query, 'password');
 
         if(response != null) {
-            localStorage.setItem('email', req.body.email);
-            res.redirect('/user_profile');
-
+            bcrypt.compare(pw, dbPass.password, function(error, equal){
+                if(equal){
+                    localStorage.setItem('email', req.body.email);
+                    res.redirect('/user_profile');
+                    console.log('Password is valid and user is logged in.');
+                }else{
+                    res.render('login', {error: "Email does not exist or password does not match."});
+                    console.log('bcrypt.compare() returned false. Password is not valid and user is not logged in.');
+                }
+            })
+            
         } else {
             res.render('login', {error: "Email does not exist or password does not match."});
         }
@@ -45,6 +58,9 @@ const securityController = {
         var name = req.body.username;
         var desc = req.body.description;
         var type = req.body.account_type;
+
+        var hashPass = await bcrypt.hash(pw, saltRounds);
+        pw = hashPass;
 
         var user = {
             email: email,
--- mco-phase2-ver2/models/user.js
+++ mco-phase2-ver2/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
 const fs = require('fs');
@@ -30,5 +30,16 @@ const UserSchema = new Schema({
         default: fs.readFileSync('./public/images/default_profile.png').toString('base64')
     }
 });
+
+UserSchema.pre('insertOne', async function(req, res){
+    try {
+        console.log('Hashing User Password');
+        const salt = await bcrypt.genSalt(10);
+        const hashPw = await bcrypt.hash(this.password, salt);
+        this.password = hashPw;
+    } catch (error) {
+        console.log("Password was not able to be hashed. Invalid user details.");
+    }
+})
 
 module.exports = mongoose.model('User', UserSchema);
