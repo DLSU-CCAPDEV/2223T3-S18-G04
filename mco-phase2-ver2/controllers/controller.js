33423f61515d6217363d57434681046d122e2ab4
AntonioLuizVeloso <92433849+AntonioLuizVeloso@users.noreply.github.com>
2023-08-06 21:03:46 +0800

    added about redirect

--- mco-phase2-ver2/controllers/controller.js
+++ mco-phase2-ver2/controllers/controller.js
@@ -1,6 +1,11 @@
 const controller = {
     getAbout: function (req, res) {
+        if(!req.session.email){
+            res.redirect('login');
+            console.log('User not logged in. Redirected')
+        }else{
         res.render('about');
+        }
     }
 }
 
